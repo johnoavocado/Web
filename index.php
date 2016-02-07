f3e628a9e6cabee8f37e10c9a8626bad225884de
John O'Donnell <johnoavocado@gmail.com>
2016-02-07 20:03:37 +0000

    Restructuring of the basic files.

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<link rel="stylesheet" type="text/css" href="assets/css/ja.css">
+
+</head>
+
+
+<body>
+	<title>JohnoAvocado</title>
+	
+
+	<h1>JohnoAvocado</h1>
+	<p>Lets try and get some output from a script!</p>
+    
+	<h2>Script output:</h2>
+	<p id="scriptout"></p>	
+</body>
+
+
+</html>
