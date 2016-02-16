f920480b69e4a91c21efb880d500797f7b44d972
John O'Donnell <johnoavocado@gmail.com>
2016-02-16 01:26:03 +0000

    Flipping to IAA page.

---
+++ avocado.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<link rel="stylesheet" type="text/css" href="assets/css/ja.css">
+
+</head>
+
+<body>
+
+<?php
+    // Grab thre html segments
+    include 'assets/html/header.html';
+    include 'assets/html/scriptout.html';
+?>
+	
+	<div class="ja fat">
+	    <a href="index.php"><img id="logo" src="https://drive.google.com/uc?id=0B5f0t1OksMN8aGdtOE9FSUJOMW8" /></a>
+    </div>
+    
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -1,22 +1,94 @@
+
 <!DOCTYPE html>
 <html>
 
 <head>
-<link rel="stylesheet" type="text/css" href="assets/css/ja.css">
-
+<link rel="stylesheet" type="text/css" href="assets/css/iaa.css">
+<title>Irish Autism Action</title>
 </head>
 
 <body>
 
-<?php
-    // Grab thre html segments
-    include 'assets/html/header.html';
-    include 'assets/html/scriptout.html';
-?>
-	
-	<div class="ja fat">
-	    <a href="index.php"><img id="logo" src="https://drive.google.com/uc?id=0B5f0t1OksMN8aGdtOE9FSUJOMW8" /></a>
+<div class="social">
+    <div class="socialicon">
+        <img src="https://g.twimg.com/Twitter_logo_blue.png">
     </div>
+    <div class="socialicon">
+        <img src="https://www.seeklogo.net/wp-content/uploads/2011/06/facebook-icon-logo-vector.png">
+    </div>
+    <div class="socialsearch">Search...</div>
+</div>
+
+<div class="logo">
+    <img src="http://www.autismireland.ie/assets/templates/autismireland/images/logo.jpg">
+</div>
+
+<div class="topmenu">
+<ul>
+  <li><a href="#">Home</a></li>
+  <li><a href="#">News</a></li>
+  <li><a href="#">Contact</a></li>
+  <li><a href="#">About</a></li>
+</ul>
+</div>
+
+<div class = "mmenu">
+
+    <div class="mmenuitem" id="aboutaut">
+        <div class="rotate"><h2>Autism</h2></div>
+        </div>
+    <div class="mmenuitem" id="aboutiaa">
+        <div class="rotate"><h2>IAA<h2></div>
+        </div>
+    <div class="mmenuitem" id="photo">
+        <img src="http://www.autismireland.ie/assets/images/gallery/WAAD2015/150402-T1-015.jpg" > 
+    </div>
+    <div class="mmenuitem" id="container">
+        <div class="mmenuitem" id="donate"><h2>Donate</h2></div>
+        <div class="mmenuitem" id="money"><h2>Impact</h2></div>
+    </div>
+
+</div>
+
+<div class="tilecontainer">
+    <div class="tilecol1">
+        <div class="tile">
+            Autism
+        </div>
+        <div class="tile">
+            ID Card
+        </div>
+        <div class="tile">
+            Therapy
+        </div>
+    </div>
+    <div class="tilecol2">
+        <div class="tile">
+            Twitter feed?
+        </div>
+        <div class="tile">
+            An event!
+        </div>
+        <div class="tile">
+            Another event :P
+        </div>
+    </div>
+    <div class="tilecol3">
+        <div class="tile">
+            Blue nose day
+        </div>
+        <div class="tile">
+            A photo!
+        </div>
+        <div class="tile">
+            Links
+        </div>
+    </div>
+</div>
+	
+<div>
+    
+</div>
     
 </body>
 
